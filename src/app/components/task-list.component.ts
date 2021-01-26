b596ead6c258b3fa4d86eb9d4369c2aa2e878731
Erika Imperial <imperial.erikajane@gmail.com>
2021-01-27 00:08:23 +0800

    Composite component tutorial

---
+++ src/app/components/task-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../models/task.model';
+
+@Component({
+    selector: 'app-task-list',
+    templateUrl: './task-list.component.html'
+})
+export class TaskListComponent implements OnInit {
+    tasksInOrder: Task[] = [];
+    @Input() loading = false;
+
+    @Output() onPinTask: EventEmitter<any> = new EventEmitter();
+    @Output() onArchiveTask: EventEmitter<any> = new EventEmitter();
+
+    @Input()
+    set tasks(arr: Task[]) {
+        this.tasksInOrder = [
+            ...arr.filter(t => t.state === 'TASK_PINNED'),
+            ...arr.filter(t => t.state !== 'TASK_PINNED')
+        ];
+    }
+
+
+    constructor() {}
+    ngOnInit() {}
+}

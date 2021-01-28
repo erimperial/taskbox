f509b1468569a1b3bf3634897d71a21897b148c9
Erika Imperial <imperial.erikajane@gmail.com>
2021-01-28 11:06:12 +0800

    Remove unused stories

--- src/app/components/task-list.component.spec.ts
+++ src/app/components/task-list.component.spec.ts
@@ -1,26 +1,26 @@
-import {render} from '@testing-library/angular';
-import {TaskListComponent} from './task-list.component';
-import {TaskComponent} from './task.component';
-import {withPinnedTasksData} from './task-list.stories';
-import { PureTaskListComponent } from './pure-task-list.component';
+// import {render} from '@testing-library/angular';
+// import {TaskListComponent} from './task-list.component';
+// import {TaskComponent} from './task.component';
+// import {withPinnedTasksData} from './task-list.stories';
+// import { PureTaskListComponent } from './pure-task-list.component';
 
-describe('PureTaskList component', () => {
-    it('renders pinned tasks at the start of the list', async () => {
-        const mockedActions = jest.fn();
-        const tree = await render(PureTaskListComponent, {
-            declarations: [TaskComponent],
-            componentProperties: {
-                tasks: withPinnedTasksData,
-                loading: false,
-                onPinTask: {
-                    emit: mockedActions
-                } as any,
-                onArchiveTask: {
-                    emit: mockedActions
-                } as any
-            }
-        });
-        const component = tree.fixture.componentInstance;
-        expect(component.tasksInOrder[0].id).toBe('6');
-    });
-});
+// describe('PureTaskList component', () => {
+//     it('renders pinned tasks at the start of the list', async () => {
+//         const mockedActions = jest.fn();
+//         const tree = await render(PureTaskListComponent, {
+//             declarations: [TaskComponent],
+//             componentProperties: {
+//                 tasks: withPinnedTasksData,
+//                 loading: false,
+//                 onPinTask: {
+//                     emit: mockedActions
+//                 } as any,
+//                 onArchiveTask: {
+//                     emit: mockedActions
+//                 } as any
+//             }
+//         });
+//         const component = tree.fixture.componentInstance;
+//         expect(component.tasksInOrder[0].id).toBe('6');
+//     });
+// });
--- src/app/components/task-list.stories.ts
+++ src/app/components/task-list.stories.ts
@@ -1,91 +1,91 @@
-import { moduleMetadata } from '@storybook/angular';
-import { CommonModule } from '@angular/common';
-import { TaskListComponent } from './task-list.component';
-import { TaskComponent } from './task.component';
-import { taskData, actionsData } from './task.stories';
+// import { moduleMetadata } from '@storybook/angular';
+// import { CommonModule } from '@angular/common';
+// import { TaskListComponent } from './task-list.component';
+// import { TaskComponent } from './task.component';
+// import { taskData, actionsData } from './task.stories';
 
-export default {
-    title: 'TaskList',
-    excludeStories: /.*Data$/,
-    decorators: [
-        moduleMetadata({
-            declarations: [TaskListComponent, TaskComponent],
-            imports: [CommonModule]
-        })
-    ]
-};
+// export default {
+//     title: 'TaskList',
+//     excludeStories: /.*Data$/,
+//     decorators: [
+//         moduleMetadata({
+//             declarations: [TaskListComponent, TaskComponent],
+//             imports: [CommonModule]
+//         })
+//     ]
+// };
 
-export const defaultTasksData = [
-    {...taskData, id: '1', title: 'Task 1'},
-    { ...taskData, id: '2', title: 'Task 2' },
-    { ...taskData, id: '3', title: 'Task 3' },
-    { ...taskData, id: '4', title: 'Task 4' },
-    { ...taskData, id: '5', title: 'Task 5' },
-    { ...taskData, id: '6', title: 'Task 6' }
-];
+// export const defaultTasksData = [
+//     {...taskData, id: '1', title: 'Task 1'},
+//     { ...taskData, id: '2', title: 'Task 2' },
+//     { ...taskData, id: '3', title: 'Task 3' },
+//     { ...taskData, id: '4', title: 'Task 4' },
+//     { ...taskData, id: '5', title: 'Task 5' },
+//     { ...taskData, id: '6', title: 'Task 6' }
+// ];
 
-export const withPinnedTasksData = [
-    ...defaultTasksData.slice(0, 5),
-    {id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED'}
-];
+// export const withPinnedTasksData = [
+//     ...defaultTasksData.slice(0, 5),
+//     {id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED'}
+// ];
 
-export const Default = () => ({
-    component: TaskListComponent,
-    template: `
-    <div style="padding: 3rem">
-        <app-task-list 
-        [tasks]="tasks" 
-        (onPinTask)="onPinTask($event)" 
-        (onArchiveTask)="onArchiveTask($event)">
-        </app-task-list>
-    </div>
-    `,
-    props: {
-        tasks: defaultTasksData,
-        onPinTask: actionsData.onPinTask,
-        onArchiveTask: actionsData.onArchiveTask
-    }
-});
+// export const Default = () => ({
+//     component: TaskListComponent,
+//     template: `
+//     <div style="padding: 3rem">
+//         <app-task-list 
+//         [tasks]="tasks" 
+//         (onPinTask)="onPinTask($event)" 
+//         (onArchiveTask)="onArchiveTask($event)">
+//         </app-task-list>
+//     </div>
+//     `,
+//     props: {
+//         tasks: defaultTasksData,
+//         onPinTask: actionsData.onPinTask,
+//         onArchiveTask: actionsData.onArchiveTask
+//     }
+// });
 
-export const WithPinnedTasks = () => ({
-    component: TaskListComponent,
-    template: `
-    <div style="padding: 3rem">
-        <app-task-list 
-        [tasks]="tasks" 
-        (onPinTask)="onPinTask($event)" 
-        (onArchiveTask)="onArchiveTask($event)">
-        </app-task-list>
-    </div>
-    `,
-    props: {
-        tasks: withPinnedTasksData,
-        onPinTask: actionsData.onPinTask,
-        onArchiveTask: actionsData.onArchiveTask
-    }
-});
+// export const WithPinnedTasks = () => ({
+//     component: TaskListComponent,
+//     template: `
+//     <div style="padding: 3rem">
+//         <app-task-list 
+//         [tasks]="tasks" 
+//         (onPinTask)="onPinTask($event)" 
+//         (onArchiveTask)="onArchiveTask($event)">
+//         </app-task-list>
+//     </div>
+//     `,
+//     props: {
+//         tasks: withPinnedTasksData,
+//         onPinTask: actionsData.onPinTask,
+//         onArchiveTask: actionsData.onArchiveTask
+//     }
+// });
 
-export const Loading = () => ({
-    template: `
-    <div style="padding: 3rem">
-        <app-task-list 
-        [tasks]="[]" 
-        loading="true" 
-        (onPinTask)="onPinTask($event)" 
-        (onArchiveTask)="onArchiveTask($event)">
-        </app-task-list>
-    </div>
-    `
-});
+// export const Loading = () => ({
+//     template: `
+//     <div style="padding: 3rem">
+//         <app-task-list 
+//         [tasks]="[]" 
+//         loading="true" 
+//         (onPinTask)="onPinTask($event)" 
+//         (onArchiveTask)="onArchiveTask($event)">
+//         </app-task-list>
+//     </div>
+//     `
+// });
 
-export const Empty = () => ({
-    template: `
-    <div style="padding: 3rem">
-        <app-task-list 
-        [tasks]="[]" 
-        (onPinTask)="onPinTask($event)" 
-        (onArchiveTask)="onArchiveTask($event)">
-        </app-task-list>
-    </div>
-     `
-});
+// export const Empty = () => ({
+//     template: `
+//     <div style="padding: 3rem">
+//         <app-task-list 
+//         [tasks]="[]" 
+//         (onPinTask)="onPinTask($event)" 
+//         (onArchiveTask)="onArchiveTask($event)">
+//         </app-task-list>
+//     </div>
+//      `
+// });

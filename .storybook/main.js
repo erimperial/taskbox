d71981f47a05473b8fb5deaea578d0598fade09c
Erika Imperial <imperial.erikajane@gmail.com>
2021-01-28 11:54:33 +0800

    Addons Tutorial

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../src/app/components/**/*.stories.ts'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes']
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-knobs']
 }
--- src/app/components/task.stories.ts
+++ src/app/components/task.stories.ts
@@ -1,8 +1,13 @@
 import { action } from '@storybook/addon-actions';
+import { withKnobs, object } from '@storybook/addon-knobs';
 import { TaskComponent } from './task.component';
+
+const longTitle = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
 export default {
     title: 'Task',
-    excludeStories: /.*Data$/
+    excludeStories: /.*Data$/,
+    decorators: [withKnobs]
 };
 
 export const actionsData = {
@@ -20,7 +25,7 @@ export const taskData = {
 export const Default = () => ({
     component: TaskComponent,
     props: {
-        task: taskData,
+        task: object('task', {...taskData}),
         onPinTask: actionsData.onPinTask,
         onArchiveTask: actionsData.onArchiveTask
     }
@@ -45,6 +50,15 @@ export const Archived = () => ({
             ...taskData,
             state: 'TASK_ARCHIVED'
         },
+        onPinTask: actionsData.onPinTask,
+        onArchiveTask: actionsData.onArchiveTask
+    }
+});
+
+export const LongTitle = () => ({
+    component: TaskComponent,
+    props: {
+        task: {...taskData, title: longTitle},
         onPinTask: actionsData.onPinTask,
         onArchiveTask: actionsData.onArchiveTask
     }
